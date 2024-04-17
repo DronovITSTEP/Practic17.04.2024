12c64ee559951891f3c5f3181a6cffdc24f4d86e
ApxNord <dronov_a@t.top-academy.ru>
2024-04-17 19:20:32 +0300

    Добавьте файлы проекта.

---
+++ Practic17.04.2024/MyOtherClass.cpp
@@ -0,0 +1,10 @@
+#include "MyOtherClass.h"
+
+void MyOtherClass::print() {
+	cout << x << " " << y;
+}
+MyOtherClass::MyOtherClass(int x, int y) {
+	// this для уточнения кто является переменной класса
+	this->x = x;
+	this->y = y;
+}
---
+++ Practic17.04.2024/Practic17.04.2024.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+// класс
+class MyClass {
+private:
+    // поля класса
+    int x;
+    int y;
+    char c;
+public:
+    // методы класса
+    void func1(){}
+    int func2() {}
+    void func3(int s, char* t){}
+    
+    // конструктор
+    MyClass(){} // по умолчанию
+
+};
+
+int main()
+{
+    MyClass obj;
+    obj.func1();
+}
